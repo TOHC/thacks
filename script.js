a114bc7ae3d518ed68b1af8aaa1c2f09bb9204ff
Curtis Chong <curtischong5@gmail.com>
2017-08-12 23:57:32 -0400

    stashing

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ $(document).ready(collapseNavbar);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
+    $('page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
